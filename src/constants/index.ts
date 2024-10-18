e65ce9ada82188c502efe3854781fe20f4e3e467
jmanhype <straughterguthrie@gmail.com>
2024-10-18 15:52:41 -0500

    Please enter the commit message for your changes. Lines starting

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 //const DEV_URL = "http://127.0.0.1:3000";
 //const TEST_URL = "https://backend-ai-chat-audio-influencer.vercel.app";
-const PROD_URL = "https://www.one2one.ai:8010"; // Updated backend URL
+const PROD_URL = "http://213.173.108.102:8080";
 //const ADMIN_DEV_URL = "http://localhost:5174";
 const ADMIN_PROD_URL = "https://admin.one2one.ai";
  //"https://enlighten-me-api-env.eba-gcie6i22.ap-south-1.elasticbeanstalk.com";
